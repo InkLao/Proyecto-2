8adbf07d78c2010456df787648129eb454747c79
Juliomst <juliosotomayor121@gmail.com>
2023-11-01 01:01:43 -0700

    entidad vehiculos
    entidad vehiculos creada

--- registroPlacas/src/main/java/dominio/Vehiculos.java
+++ registroPlacas/src/main/java/dominio/Vehiculos.java
@@ -4,10 +4,34 @@
  */
 package dominio;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Objects;
+
 /**
  *
  * @author HP
  */
+@Entity
+@Table(name = "vehiculos")
 public class Vehiculos {
+    
+    @Id
+    @Column(name = "serie", length = 255)
+    private String serie;
+    
+    @Column(name = "modelo", nullable = false, length = 255)
+    private String modelo;
+    
+    @Column(name = "color", nullable = false, length = 255)
+    private String color;
+    
+    @Column(name = "linea", nullable = false, length = 255)
+    private String linea;
+    
+    @Column(name = "marca", nullable = false, length = 255)
+    private String marca;
+    
     
 }
