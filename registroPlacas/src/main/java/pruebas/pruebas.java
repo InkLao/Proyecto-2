ba692995d52848479232271551e1273d22492780
jaiRHZ <133193972+jaiRHZ@users.noreply.github.com>
2023-11-01 01:34:02 -0700

    Persistencia
    Se ha creado el paquete de persistencia

---
+++ registroPlacas/src/main/java/pruebas/pruebas.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebas;
+
+import dominio.Persona;
+import java.util.Calendar;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Jairo G. Rodriguez Hernandez 00000213248
+ */
+public class pruebas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistencia");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        
+        Persona nuevoUsuario = new Persona("1234567891011", "Daniel", "Alamea", "Lopez", Calendar.getInstance(), Boolean.FALSE, "123456789");
+        em.persist(nuevoUsuario);
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
+        
+    }
+    
+}
