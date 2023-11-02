64b5dbb0e9cae1e8404391bb3d5528dec8107179
jaiRHZ <133193972+jaiRHZ@users.noreply.github.com>
2023-11-01 19:10:19 -0700

    Paquete Interfaces
    Paquete DAO fue renombrado a Interfaces

--- registroPlacas/src/main/java/interfaces/IPersonaDAO.java
+++ registroPlacas/src/main/java/interfaces/IPersonaDAO.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package DAO;
+package interfaces;
 
 import dominio.Persona;
 import java.util.List;
@@ -11,7 +11,7 @@ import java.util.List;
  *
  * @author 
  */
-public interface personaDAO {
+public interface IPersonaDAO {
     // Obtiene la persona por ID
     Persona getPersona(int id);
     // Lista de todas las personas
